d09d2c82218a0e1305a8e0ebb3bed6dddc74c966
Wenying Chang <changwenyingnl@gmail.com>
2024-08-13 08:32:09 +0200

    fix:/tidy tooltip component

--- src/Tooltip/customTooltip.jsx
+++ src/Tooltip/customTooltip.jsx
@@ -5,15 +5,15 @@ const CustomTooltip = styled(({ className, ...props }) => (
   <Tooltip {...props} classes={{ popper: className }} placement="top" arrow />
 ))(() => ({
   '& .MuiTooltip-tooltip': {
-    backgroundColor: '#333', // Tooltip background color
-    color: '#fff', // Tooltip text color
-    fontSize: '12px', // Tooltip text size
-    borderRadius: '4px', // Tooltip border radius
-    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)', // Custom box shadow
-    padding: '12px 12px', // Padding inside the tooltip
+    backgroundColor: '#333', 
+    color: '#fff', 
+    fontSize: '12px', 
+    borderRadius: '4px', 
+    boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)', 
+    padding: '12px 12px', 
   },
   '& .MuiTooltip-arrow': {
-    color: '#333', // Arrow color to match the tooltip background
+    color: '#333', 
   },
 }));
 
