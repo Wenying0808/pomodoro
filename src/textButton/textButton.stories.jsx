c2b93d4a217a958ec921a99fc6b7d8dd0e85ea3a
Wenying Chang <changwenyingnl@gmail.com>
2024-08-15 20:30:05 +0200

    feat:/add textButton story

---
+++ src/textButton/textButton.stories.jsx
@@ -0,0 +1,12 @@
+import TextButton from "./textButton";
+
+export default {
+    title: 'Components/Text Button',
+    component: TextButton,
+};
+
+export const Default = {
+    args:{
+        label: "Text Button"
+    }
+};
